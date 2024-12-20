87db043934c9bd4859c50b11cbb7bc9011b8961b
sohelhussain <sohelhussaing@gmail.com>
2024-12-20 18:34:16 +0530

    feat: INIT THE BACKEND AND CONNET DATABASE & CONNECTION POOL

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import { Hono } from 'hono'
+import {PrismaClient} from '@prisma/client/edge'
+import {withAccelerate} from '@prisma/extension-accelerate'
+const app = new Hono()
+
+app.post('/api/v1/signup/', (c) => {
+  
+})
+app.post('/api/v1/signin/', (c) => {
+  
+})
+app.post('/api/v1/blog', (c) => {
+  
+})
+app.post('/api/v1/blog', (c) => {
+  
+})
+app.post('/api/v1/blog/:id', (c) => {
+  
+})
+
+
+
+
+
+export default app
